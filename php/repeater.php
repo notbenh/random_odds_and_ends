ed8793e17080d8a13ee3e68771a9ae29f37f4843
ben hengst <notbenh@cpan.org>
2012-10-05 13:59:02 -0700

    repeat this

---
+++ php/repeater.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>The Repeater!</title>
+  <style type='text/css'>
+    body { 
+      background-image:url('<?= $_GET['img'] ? $_GET['img'] : "/media/funny-gifs-pug-fangs.gif"?>');
+  </style>
+</head>
+<body>
+</body>
+</html>
